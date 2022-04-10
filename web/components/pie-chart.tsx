f1226753eeb44479ec8b412ac41b3412e0f4c6a3
udbhav1 <umuthakana@gmail.com>
2022-04-09 23:31:33 -0400

    add pie chart styles

--- web/components/pie-chart.tsx
+++ web/components/pie-chart.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Pie } from "@visx/shape";
+import {
+  Tooltip,
+  defaultStyles,
+} from "@visx/tooltip";
 import { Group } from "@visx/group";
 import { scaleOrdinal } from "@visx/scale";
 import { letterFrequency } from "@visx/mock-data";
+import { useState } from "react";
 
 const letters = letterFrequency.slice(0, 4);
 const frequency = (d: any) => d.frequency;
@@ -35,48 +40,63 @@ const PieChart = ({ width, height, margin = defaultMargin }: PieChartProps) => {
   const left = centerX + margin.left;
   const pieSortValues = (a: number, b: number) => b - a;
 
+  const [pieHover, setPieHover] = useState("TEST");
+
   return (
-    <svg width={width} height={height}>
-      <Group top={top} left={left}>
-        <Pie
-          data={letters}
-          pieValue={frequency}
-          pieSortValues={pieSortValues}
-          outerRadius={radius}
-          innerRadius={2*radius / 3}
-          padAngle={0.015}
-          cornerRadius={7}
-        >
-          {(pie) => {
-            return pie.arcs.map((arc, index) => {
-              const { letter } = arc.data;
-              const [centroidX, centroidY] = pie.path.centroid(arc);
-              const hasSpaceForLabel = arc.endAngle - arc.startAngle >= 0.1;
-              const arcPath = pie.path(arc);
-              const arcFill = getLetterFrequencyColor(letter);
-              return (
-                <g key={`arc-${letter}-${index}`}>
-                  <path d={arcPath || undefined} fill={arcFill} />
-                  {hasSpaceForLabel && (
-                    <text
-                      x={centroidX}
-                      y={centroidY}
-                      dy=".33em"
-                      fill="#ffffff"
-                      fontSize={22}
-                      textAnchor="middle"
-                      pointerEvents="none"
-                    >
-                      {arc.data.letter}
-                    </text>
-                  )}
-                </g>
-              );
-            });
-          }}
-        </Pie>
-      </Group>
-    </svg>
+    <div style={{position: "relative"}}>
+      <div>
+        {/* <p style={{position: "absolute", fontSize: "30px", color: "ebebed", height: "100%", width: "100%", display: "flex", alignContent: "center", alignItems: "center", justifyContent: "center", zIndex: "1"}}>TESTING</p> */}
+        <p style={{position: "absolute", fontSize: "30px", color: "ebebed", left: "189px", top: "177px"}}>{pieHover}</p>
+      </div>
+      <svg width={width} height={height} style={{zIndex: "10"}}>
+        <Group top={top} left={left}>
+          <Pie
+            data={letters}
+            pieValue={frequency}
+            pieSortValues={pieSortValues}
+            outerRadius={radius}
+            innerRadius={2*radius / 3}
+            padAngle={0.015}
+            cornerRadius={7}
+          >
+            {(pie) => {
+              return pie.arcs.map((arc, index) => {
+                const { letter } = arc.data;
+                const [centroidX, centroidY] = pie.path.centroid(arc);
+                const hasSpaceForLabel = arc.endAngle - arc.startAngle >= 0.1;
+                const arcPath = pie.path(arc);
+                const arcFill = getLetterFrequencyColor(letter);
+                return (
+                  <g key={`arc-${letter}-${index}`} style={{zIndex: "10"}}>
+                    <path d={arcPath || undefined} fill={arcFill} className="arc" 
+                    onMouseEnter={() => {
+                      setPieHover(arc.data.letter);
+                    }}
+                    onMouseLeave={() => {
+                      setPieHover("");
+                    }}
+                    />
+                    {/* {hasSpaceForLabel && (
+                      <text
+                        x={centroidX}
+                        y={centroidY}
+                        dy=".33em"
+                        fill="#ffffff"
+                        fontSize={22}
+                        textAnchor="middle"
+                        pointerEvents="none"
+                      >
+                        {arc.data.letter}
+                      </text>
+                    )} */}
+                  </g>
+                );
+              });
+            }}
+          </Pie>
+        </Group>
+      </svg>
+    </div>
   );
 };
 
